3b6686bffa23235b23b1be4d192d87ae54dc5afb
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-04-07 21:32:44 -0400

    monday updates

--- Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
+++ Unit-2-Intro-To-C#/Day-6-Arrays-Lists/Day-6-Arrays-Lists/Program.cs
@@ -4,42 +4,62 @@
 //      add them together and display the total
 
 class Program
-{
+{   
     static void Main(string[] args)
     {
-        // Define the variables to hold the numbers we need to add
-        int number1 = 0; // Since we will be doing math it needs to be numeric
-        int number2 = 0; // Since we will be doing math it needs to be numeric
-        int number3 = 0; // Since we will be doing math it needs to be numeric
+        // Define an array to hold 3 numbers 
+        int[] numbers = new int[3];  // numbers.Length = 3; 2 is the largest allowable index
 
         // Define a variable to hold the sum of the numbers
         int sum = 0;
 
         Console.WriteLine("--- Starting program ---");
+        
+        // Normally we process an array from beginning to end 
+        // (from the first element to the last element, one at a time)
+        //
+        // a for-loop will loop through a process counting as it does so
+        // a for-loop is an excellent tool for processing an array from start to end 
+        
+        // standard syntax: for(i=0, i < arrayName.Length; i++)
+        //
+        // arrayName.Length represnets the number of elements in the array
 
-        // Ask for the numbers one at time using the C# Console object
-        //     which represents the keyboard and screen
-        // Console.ReadLine() returns a string from keyboard - cannot store in an int
-        Console.WriteLine("Please enter a number:  "); // Asking for the number
-        string theResponse; // Define a string to hold the line of input from the keyboard
-        theResponse = Console.ReadLine(); // Get a line from the keyboard
-      
-        // int.Parse(string) will convert a string to an int
-        number1 = int.Parse(theResponse);
+        //    index     loop as 
+        //    start     long as the index         increment the 
+        //    at 0      is not outside array      index for each loop
+        for (int i = 0; i < numbers.Length;        i++) // i = 0, 1, 2 inside loop and 3 when it exits the loop
+        {
+            Console.WriteLine("Please enter a number:  "); // Asking for the number
+            numbers[i] = int.Parse(Console.ReadLine());    // get a string from keyboard
+        }                                                  // convert it to an int
+                                                           // store it in number[i] 
 
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number2 = int.Parse(theResponse);
-
-        Console.WriteLine("Please enter a number:  ");
-        theResponse = Console.ReadLine(); // Reuse theResponse define above
-        number3 = int.Parse(theResponse);
+                                                           
+                                                           
+        // Verify the array received the numbers correctly
+        // Go through the array one element at a time and display the element
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Element #"+ i + " is: " + numbers[i]);
+            sum += numbers[i]; // add the current numbers in the array to sum
+        }
 
         // Add the numbers together create a sum/total
-        sum = number1 + number2 + number3;
+        // sum = number1 + number2 + number3;
        
         // Tell the requester the sum/total (display)
         Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average : " + (double)sum / numbers.Length);
+        
+        // sum / number.length
+        // int / int ---> integer arithmetic - divide gives two parts quotient and remainder
+        //    int                                   7 / 3 - quotient = 2 remainder = 1
+        
+        // (double) sum / numbers.Length
+        // convert sum
+        // to double  / int  ----> floating point arithmetic 
+        //      double (2.333333)
 
         Console.WriteLine("--- Ending program ---");
     }
