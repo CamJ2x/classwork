9339abc5add78e24a8e17b1fb9bf0d9615279f69
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-06-23 18:41:39 -0400

    blah blah blah

---
+++ Unit-6-HTML-CSS/TypeScript/project1/src/index.ts
@@ -0,0 +1,11 @@
+// typescript file goes here
+console.log("Welcome to My First TypeScript")
+/*
+
+
+*/
+interface Student{
+    studentName : string;
+    className   : string;
+    startDate   : number; //yyyymmdd
+}
