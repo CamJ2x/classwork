757cc0041e098a8912c53b0aa4a0b129a2140dc3
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-06-26 18:30:55 -0400

    loose code

--- Unit-7-Angular/Day-1-formExample/src/app/app.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/app.component.ts
@@ -9,8 +9,8 @@ import { FormExampleComponent } from './components/form-example/form-example.com
   standalone: true,
   // imports identifies external resources, like components, used in the app
   imports: [RouterOutlet, FormExampleComponent],  // Note the name has Component
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html' // The file containing the html for the component 
+  styleUrl: './app.component.css'     // The file containing the css for the component 
 })
 // Allows processes outside this component to access things defined in the component 
 // To use One-Way Binding for data defined in this code in the HTML:  {{variable-name}}
