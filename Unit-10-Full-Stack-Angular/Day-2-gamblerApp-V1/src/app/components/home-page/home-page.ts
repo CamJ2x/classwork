a7930124ad4d060a77a8fa85fda8f7104431d3e0
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-08-18 20:33:14 -0400

    monday18

--- Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/components/home-page/home-page.ts
+++ Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/components/home-page/home-page.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { GamblerDataLayout } from '../../interfaces/GamblerDataLayout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
 export class HomePage {
+  theGamblers : GamblerDataLayout [] = [
+  {id : 1, address : "Detroit",name : "Madonna",birthDate : "01/02/1963", salary : 1000000},
+  {id : 11, address : "Tupelo",name : "Elvis",birthDate : "09/02/1943", salary : 9868000},
+  {id : 99, address : "Corleone",name : "Vito",birthDate : "12/24/1999", salary : 58000}
+  
+  ]
 
 }
---
+++ Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/interfaces/GamblerDataLayout.ts
@@ -0,0 +1,8 @@
+export interface GamblerDataLayout {
+    id        : number
+    address   : string
+    name      : string
+    birthDate : string
+    salary    : number
+
+}
