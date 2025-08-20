4c637576d55d2f0cc66a83cfb624fac544395666
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-08-20 17:18:03 -0400

    wed20

--- Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/services/gambler-api-service.ts
+++ Unit-10-Full-Stack-Angular/Day-2-gamblerApp-V1/src/app/services/gambler-api-service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamblerApiService {
+  private theSeverURL : string  = "ht"
   
 }
